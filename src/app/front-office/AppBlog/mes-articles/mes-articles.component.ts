686200a658991ae92fc5852eed2b77a5de92a4ae
MADANI Mounib <mounib.madani@esprit.tn>
2022-07-26 20:27:08 +0200

    last

--- src/app/front-office/AppBlog/mes-articles/mes-articles.component.ts
+++ src/app/front-office/AppBlog/mes-articles/mes-articles.component.ts
@@ -9,18 +9,25 @@ import { BlogService } from 'src/app/shared/services/blog.service';
 })
 export class MesArticlesComponent implements OnInit {
   articles :  Article[];
-  hide = true;
+  empty : Boolean;
   constructor(private blogservice : BlogService) { }
 
   ngOnInit(): void {
     this.blogservice.getmyList().subscribe(
       (data: Article[]) => 
           this.articles = data
+
        
    );
   }
   deleteart(id: String){
     this.blogservice.delete(id).subscribe();
     alert("Article supprimé, veuillez recharger la page!");
+  }
+  isEmpty(){
+    if(this.articles.length==0){
+        return true;
+    } 
+    return false;
   }
 }
